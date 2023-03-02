a46fb5f54935c7fa795293abd1fb9df3559b549a
JoeLoera <joeloera7@gmail.com>
2023-03-02 10:03:29 -0800

    Tasks can now be added in JavaScrpit + arrays

--- js/app.js
+++ js/app.js
@@ -1,5 +1,42 @@
 const taskContainer = document.querySelector(".task-container");
 
-function render() {}
+let tasks = [
+  {
+    name: "Practice CSS Animations",
+    priority: 1,
+  },
+  {
+    name: "Dev Community Work",
+    priority: 4,
+  },
+  {
+    name: "Algorithm Studies",
+    priority: 3,
+  },
+];
 
+//sort by priority
+const descendingTasks = tasks.sort(
+  (taskA, taskB) => taskA.priority - taskB.priority
+);
+
+function render() {
+  descendingTasks.forEach((task) => {
+    const taskBlock = document.createElement("div");
+    const deleteElement = document.createElement("p");
+    const title = document.createElement("p");
+    const controller = document.createElement("button");
+
+    taskBlock.classList.add("task-block");
+    deleteElement.classList.add("delete-icon");
+    controller.classList.add("controller-button");
+
+    deleteElement.textContent = "🅇";
+    title.textContent = task.name;
+    controller.textContent = "START";
+
+    taskBlock.append(deleteElement, title, controller);
+    taskContainer.append(taskBlock);
+  });
+}
 render();
